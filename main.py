45fa1e21f7fdc28d9c1d743266ac1ed8895688c1
moe3344e <mohamad.hammoud1@ucalgary.ca>
2022-10-03 12:20:29 -0600

    four functions

--- main.py
+++ main.py
@@ -12,4 +12,17 @@ def myDiv( user_1,user_2):
     """
     user_3 = user_1 / user_2
     return (user_3)
-    
+def myAdd( user_1,user_2):
+    """user defined function return the result of addition of two integers
+    parameters : two 
+    returns integer value
+    """
+    user_3 = user_1 + user_2
+    return (user_3)
+def myMul( user_1,user_2):
+     """user defined function return the result of multiplication of two integers
+    parameters : two 
+    returns integer value
+    """
+    user_3 = user_1 * user_2
+    return (user_3)
