f5a5ec380c55d0588ee39d20cbaad6ca061a3152
moe3344e <mohamad.hammoud1@ucalgary.ca>
2022-10-03 12:16:53 -0600

    de

---
+++ main.py
@@ -0,0 +1,15 @@
+def mySub( user_1,user_2): 
+    """user defined function return the result of subtraction of two integers
+    parameters : two 
+    returns integer value
+    """
+    user_3 = user_1 - user_2
+    return (user_3)
+def myDiv( user_1,user_2): 
+     """user defined function return the result of division of two integers
+    parameters : two 
+    returns integer value
+    """
+    user_3 = user_1 / user_2
+    return (user_3)
+    
