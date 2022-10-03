5aa464299f3961d04a58408796de717a1f423460
aryan312216 <aryan.sharma2@ucalgary.ca>
2022-10-03 12:45:20 -0600

    main and disply funcs

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 def main():
-firstValue = input("Enter your first value: ")
-firstOperator = input("Enter your first operator: ")
-secondValue = input("Enter your second value: ")
-SecondOperator = input("Enter your second operator: ")
-thirdValue = input("Enter your third value: ")
-while()
+    i=0
+    while(i==0):
+        firstValue = input("Enter your first value: ")  
+        firstOperator = input("Enter your first operator: ")
+        secondValue = input("Enter your second value: ")
+        secondOperator = input("Enter your second operator: ")
+        thirdValue = input("Enter your third value: ")
+        if( valOperator(firstOperator)==0 or valOperator(secondOperator)==0 or valInteger(firstValue)==0 or valInteger(secondValue)==0 or valInteger(thirdValue)==0 ):
+            i=0
+            print("One of the values were entered incorrectly")
+        else:
+            i=1
+    
 def display(list1 , list2):
     """A function that prints the expression and displays the final result.
 
