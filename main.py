5507352c068dea4a1657df08996382c0e32a1c9b
aryan312216 <aryan.sharma2@ucalgary.ca>
2022-10-03 12:24:54 -0600

    main and display functions

---
+++ main.py
@@ -0,0 +1,17 @@
+def main():
+firstValue = input("Enter your first value: ")
+firstOperator = input("Enter your first operator: ")
+secondValue = input("Enter your second value: ")
+SecondOperator = input("Enter your second operator: ")
+thirdValue = input("Enter your third value: ")
+while()
+def display(list1 , list2):
+    """A function that prints the expression and displays the final result.
+
+        Parameters: list1: List of Values
+                    list2: List of Operators
+                    finalExpression: The expression with 3 values and 2 operators
+        Return: None
+
+        """
+    print(list1[0], list2[0], list1[1], list2[1], list1[2], list2[2], list1[3])
